3497d4acb5c3ac227e606dff68112c68fed8f36e
JoaoNeto <joaonetovhc@gmail.com>
2025-06-02 23:05:14 -0300

    criando a conexao com o banco o cadastro de candidatos para teste

--- api/conexao.php
+++ api/conexao.php
@@ -1,16 +1,16 @@
 <?php
-$host = 'localhost';       // ou IP do servidor
-$dbname = 'baracity';      // nome do banco de dados
-$user = 'root';            // usuário do MySQL
-$pass = '';                // senha (coloque se tiver)
+$host = 'localhost';
+$dbname = 'baracity';
+$user = 'root';
+$pass = '123456';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
-    // Ativa os erros como exceções
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 } catch (PDOException $e) {
     http_response_code(500);
-    echo json_encode(['erro' => 'Erro na conexão com o banco de dados: ' . $e->getMessage()]);
+    echo json_encode(['erro' => 'Erro na conexão com o banco de dados: ' . $e -> getMessage()]);
     exit;
 }
 ?>
